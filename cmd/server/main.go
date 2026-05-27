221295fbbf40fd6cec2b8e71f41fc93c2b5f269e
nrybakovskiy <nrybakovskiy@ozon.ru>
2026-05-27 23:12:57 +0300

    first inc

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/{type}/{name}/{value}", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		t := r.PathValue("type")
+		n := r.PathValue("name")
+		v := r.PathValue("value")
+
+		if n == "" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		switch t {
+		case "gauge":
+			_, err := strconv.ParseFloat(v, 64)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		case "counter":
+			_, err := strconv.ParseInt(v, 10, 64)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			return
+
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		panic(err)
+	}
+}
+
+type MemStorage struct {
+	storageGauge   map[string]float64
+	storageCounter map[string]int64
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Flash0673/metrics-go
+
+go 1.25.9
